f36d8e9498f95c3e9d45c4dc17d54760f6d6b5e8
VachAseyan <aseyanvache39@gmail.com>
2025-05-10 18:39:18 +0400

    Fix Movie interface properties: Change poster_path, vote_average, and release_date to required fields.

--- src/types/Movie.ts
+++ src/types/Movie.ts
@@ -1,9 +1,9 @@
 export interface Movie {
   id: number;
   title: string;
-  poster_path?: string | null;
-  vote_average?: number;
-  release_date?: string;
+  poster_path: string | null;
+  vote_average: number;
+  release_date: string;
 }
 
 export interface FavoriteMovie {
